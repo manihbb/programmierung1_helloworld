704edfa04a53ad8e39fd1cee7250ab6b78b8b690
Mani Shamlou <manishamlou@gmail.com>
2025-10-17 15:55:37 +0200

    Wurd VL2

---
+++ Wurf.java
@@ -0,0 +1,12 @@
+public class Wurf {
+    static void main() {
+        double zufallszahl = Math.random();
+        if (zufallszahl < 0.5) {
+            System.out.println("Kopf");
+        
+        }else {
+            System.out.println("Zahl");
+        }
+        
+    }
+}
