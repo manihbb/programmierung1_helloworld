623e73e4a5b5c5863e423e39a426284b73ebc510
Mani Shamlou <manishamlou@gmail.com>
2025-10-17 09:47:20 +0200

    Update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
+import java.lang.foreign.SymbolLookup;
+
+import javax.sound.midi.SysexMessage;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(20 + 20);
     }
 }
