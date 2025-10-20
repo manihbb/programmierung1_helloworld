095bf3079d108d497f45ae91ab03c8b93329aeb2
Mani Shamlou <manishamlou@gmail.com>
2025-10-20 09:19:56 +0200

    update

---
+++ Schaltjahre/Schaltjahr.java
@@ -0,0 +1,15 @@
+public class Schaltjahr {
+    public static void main(String[] args) {
+        int jahr = 2024;
+
+        boolean regel1 = jahr % 4 == 0;
+        boolean regel2 = jahr % 100 == 0;
+        boolean regel3 = jahr % 400 == 0;
+
+        if ((regel1 && !regel2) || regel3) {
+            System.out.println("Ja, " + jahr + " ist ein Schaltjahr.");
+        } else {
+            System.out.println("Nein, " + jahr + " ist kein Schaltjahr.");
+        }
+    }
+}
--- Wurf.java
+++ Wurf.java
@@ -1,12 +1,20 @@
 public class Wurf {
     static void main() {
-        double zufallszahl = Math.random();
-        if (zufallszahl < 0.5) {
-            System.out.println("Kopf");
-        
-        }else {
-            System.out.println("Zahl");
-        }
-        
+        //double zufallszahl = Math.random();
+        //System.out.println("Wert der Zufallszahl: " + zufallszahl);
+
+        //double richtigeBreite = zufallszahl * 6;
+        //System.out.println(richtigeBreite);
+
+        //int ohneNachkomma = (int)richtigeBreite;
+        //System.out.println(ohneNachkomma);
+
+        //int wuerfelaufgen = ohneNachkomma + 1;
+        //System.out.println(wuerfelaufgen);
+       int wuerfel;
+        do {
+            wuerfel = (int)(Math.random() * 6) + 1; // Generate random number from 1 to 6
+            System.out.println(wuerfel);
+        } while (wuerfel != 6);
     }
 }
