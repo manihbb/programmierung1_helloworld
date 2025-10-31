58d6663a831266a72522ac53dbf3abdfc404502e
Mani Shamlou <manishamlou@gmail.com>
2025-10-31 15:08:10 +0100

    VL6

---
+++ Random/Random.java
@@ -0,0 +1,11 @@
+public class Random {
+    static void main() {
+        double [] zufallszahlen = new double[50];
+
+        printArray(zufallszahlen);
+
+        for (int i = 0, i < zufallszahlen.length, i++) {
+            zufallszahlen[i] = Math.random();
+        }
+    }
+}
