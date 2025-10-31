2242ff23b27b08f37c4e986cfe96788e8bf4a1c3
Mani Shamlou <manishamlou@gmail.com>
2025-10-31 15:34:50 +0100

    VL6

---
+++ 06_arrays/beispiele/vorgabe/MeasuredValues.java
@@ -0,0 +1,11 @@
+static void main() {
+    double messwert1 = 20.5;
+    double messwert2 = 21.1;
+    double messwert3 = 21.3;
+    double messwert4 = 21.5;
+    double messwert5 = 20.1;
+    
+    double sum = messwert1 + messwert2 + messwert3 + messwert4 + messwert5;
+    
+    System.out.println(sum);
+}
