739a4cb2daf219b2d4e3945757cbeda2abe31071
Mani Shamlou <manishamlou@gmail.com>
2025-11-03 09:49:28 +0100

    VL7

---
+++ Eratosthenes/Eratosthenes.java
@@ -0,0 +1,9 @@
+public class Eratosthenes{
+    static void main() {
+        int[] primzahlen = primzahlenBis(100);
+    }
+
+    static int[] primzahlenBis(int zahl){
+        boolean[] geschtrichen = new boolean 
+    }
+}
